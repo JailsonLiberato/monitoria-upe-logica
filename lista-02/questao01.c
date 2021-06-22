0bf3bb616441138fafcd74c36e252a92e4ae38b2
Jailson Liberato <jailsonliberato@gmail.com>
2021-06-22 16:00:28 -0300

    Código da questão 1 finalizado.

--- lista-02/questao01.c
+++ lista-02/questao01.c
@@ -5,6 +5,16 @@ void main(){
     int numero2;
     printf(":::::: Questao 01 ::::::\n\n");
     printf("Digite o primeiro numero: ");
-  //  scanf("%d", &numero1);
+    scanf("%d", &numero1);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &numero2);
+    if(numero1 == numero2){
+        printf("\nO numero 1 e o numero 2 sao iguais");
+    }
+    else if(numero1 > numero2){
+        printf("\nO numero 1 eh maior que o numero 2");
+    }else{
+        printf("\nO numero 2 eh maior que o numero 1");
+    }
 
 }
