34c18426c51ebc4e986202f468e2f7d2f61eb180
Jailson Liberato <jailsonliberato@gmail.com>
2021-06-22 16:25:43 -0300

    Código da questão 2 finalizado.

---
+++ lista-02/questao02.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int anoNascimento;
+    int anoAtual = 2021;
+    printf(":::::: Questao 01 ::::::\n\n");
+    printf("Digite o ano de nascimento: ");
+    scanf("%d", &anoNascimento);
+
+    int diferenca = anoAtual - anoNascimento;
+
+    if(diferenca >= 16){
+        printf("\nPode votar");
+    }else{
+        printf("\nNao pode votar");
+    }
+
+    return 0;
+}
