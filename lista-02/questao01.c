510331f4b913d114661d803a8501cf381599c2e9
Jailson Liberato <jailsonliberato@gmail.com>
2021-06-22 11:33:25 -0300

    Inicializando a lista.

---
+++ lista-02/questao01.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+    int numero1;
+    int numero2;
+    printf(":::::: Questao 01 ::::::\n\n");
+    printf("Digite o primeiro numero: ");
+  //  scanf("%d", &numero1);
+
+}
